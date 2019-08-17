6d2aa4ac15923f236f0963235a0fd3f9712d31d1
jkrivonos <ja.krivonos@gmail.com>
2019-08-17 14:58:10 +0300

    переписала тернарник для установки статуса юзера на функцию

--- src/components/user-profile.js
+++ src/components/user-profile.js
@@ -1,5 +1,19 @@
 export const userProfile = (watchedFilms) =>
   `<section class="header__profile profile">
-    <p class="profile__rating">${watchedFilms > 0 ? (watchedFilms > 10 ? (watchedFilms > 20 ? `movie buff` : `fan`) : `novice`) : ``}</p>
+    <p class="profile__rating">${getUserStatus(watchedFilms)}</p>
     <img class="profile__avatar" src="images/bitmap@2x.png" alt="Avatar" width="35" height="35">
   </section>`;
+
+const getUserStatus = (watchedFilms) => {
+  if (watchedFilms > 0) {
+    if (watchedFilms > 10) {
+      if (watchedFilms >= 20) {
+        return `movie buff`;
+      }
+      return `fun`;
+    } else if (watchedFilms <= 10) {
+      return `novice`;
+    }
+  } return ``;
+};
+
